cd65bf16078941db16c74b86e1ca8977b0adef14
Ethan Barton <barton.ethan@gmail>
2020-10-12 16:19:58 -0500

    Refactor Navbar Styles and Remove CSS

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/styles";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import Snackbar from "@material-ui/core/Snackbar";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
-
-import "rc-slider/assets/index.css";
 import Slider from "rc-slider";
-import "./Navbar.css";
+import styles from "./styles/NavbarStyles";
+import "rc-slider/assets/index.css";
 
-export default class Navbar extends Component {
+
+
+class Navbar extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { format: "hex", open: false };
@@ -25,22 +27,22 @@ export default class Navbar extends Component {
 		this.setState({ open: false });
 	}
 	render() {
-		const { level, changeLevel, showingAllColors } = this.props;
+		const { level, changeLevel, showingAllColors, classes } = this.props;
 		const { format } = this.state;
 		return (
-			<header className="Navbar">
-				<div className="logo">
+			<header className={classes.Navbar}>
+				<div className={classes.logo}>
 					<Link to="/">reactcolorpicker</Link>
 				</div>
 				{showingAllColors && (
-					<div className="slider-container">
+					<div>
 						<span>Level: {level}</span>
-						<div className="slider">
+						<div className={classes.slider}>
 							<Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={changeLevel} />
 						</div>
 					</div>
 				)}
-				<div className="select-container">
+				<div className={classes.selectContainer}>
 					<Select value={format} onChange={this.handleFormatChange}>
 						<MenuItem value="hex">Hex - #ffffff</MenuItem>
 						<MenuItem value="rgb">RGB - rgb(255, 255, 255)</MenuItem>
@@ -64,3 +66,5 @@ export default class Navbar extends Component {
 		);
 	}
 }
+
+export default withStyles(styles)(Navbar);
