5fc4eba1124dee958a35f774983dccaff27797eb
Ethan Barton <barton.ethan@gmail>
2020-10-22 12:14:09 -0500

    Add bg svg to PaletteList

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,11 +1,15 @@
 import sizes from "./sizes"
+import bg from "./bg.svg"
 export default {
 	root: {
-		backgroundColor: "blue",
 		height: "100vh",
 		display: "flex",
 		alignItems: "flex-start",
-		justifyContent: "center"
+		justifyContent: "center",
+		// background by SVGBackgrounds.com
+		backgroundColor: "#000000",
+		backgroundImage: `url(${bg})`,
+		overflow: "scroll"
 	},
 	container: {
 		width: "50%",
@@ -41,7 +45,10 @@ export default {
 		},
 		[sizes.down("xs")]: {
 			gridTemplateColumns: "repeat(1, 100%)",
-			gridGap: "1rem",
+			gridGap: "1.4rem",
 		},
+	},
+	heading: {
+		fontSize: "2rem"
 	}
 };
